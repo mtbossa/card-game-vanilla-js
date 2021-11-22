68ecd5f805dc21b4180d9207441a2c22a76717e9
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2021-11-22 18:58:18 -0300

    Crated CardGame component. Created  reset.css and colors.css

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+export default function CardGame() {
+    return /*html*/ `
+        <article class="card-game">
+            <img src="images/logo-alura.png" alt="Logo da Alura">
+        </article>
+    `;
+}
