911e47d1eda9e490381958724fea7c9f1f6694fc
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2021-11-23 20:46:59 -0300

    Created PlayerArrow component.

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -3,10 +3,12 @@ import './style.css'
 import VsPlayer from '../../components/VsPlayer';
 import PlayerName from '../../components/PlayerName';
 import PlayerScore from '../PlayerScore';
+import PlayerArrow from '../../components/PlayerArrow';
 
 export default function ScoreBoard(avaliablePoints, playerScore=0) {
     return /*html*/ `
-        <header class="score-board">    
+        <header class="score-board">  
+            ${PlayerArrow(1)}  
             ${PlayerName('Player1')}     
             ${PlayerScore(avaliablePoints)} 
             ${VsPlayer()}   
