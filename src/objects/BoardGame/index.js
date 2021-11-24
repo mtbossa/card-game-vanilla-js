0ce957aef0a3d438539e443884ebfffddb3da6ae
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2021-11-23 21:43:08 -0300

    Created logic and functions for fliping the cards once two are active and change player arrow.

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -3,20 +3,39 @@ import './style.css'
 import CardFrontBack from '../../components/CardFrontBack';
 import cards from './data.js'
 
-export default function BoardGame(amountCards) {
-    console.log(cards)
+export default function BoardGame() {
+    const flipAndHideCards = ($activeCards) => {
+        $activeCards.forEach((card) => card.classList.remove('-active'));
+    }
 
+    const changePlayerArrow = () => {
+        const $playerArrow = document.querySelector('.player-arrow');
+        const currentPlayer = $playerArrow.getAttribute('data-currentPlayer');
+
+        $playerArrow.setAttribute('data-currentPlayer',(currentPlayer == 2) ? 1 : 2)
+    }
+
+    window.boardGame = {};
+    window.boardGame.handleClick = () => {
+        
+        const $activeCards = document.querySelectorAll('.card-front-back.-active');
+        
+        if ($activeCards.length === 2) {
+            setTimeout(() => {
+                flipAndHideCards($activeCards);
+                changePlayerArrow();
+            }, 1000);
+        }
+    };
+    
     const htmlCardsList = cards.map((card) => CardFrontBack(card.icon, card.altIcon));
-
     const $htmlCards = [...htmlCardsList, ...htmlCardsList]
                             .sort(() => Math.random() - 0.5)
                             .join('');
 
-    console.log($htmlCards)
-
     return /*html*/ `
-    <section class="board-game">
+    <section class="board-game" onClick="boardGame.handleClick()">
         ${$htmlCards}        
-    </div>      
+    </section>      
     `;
 }
